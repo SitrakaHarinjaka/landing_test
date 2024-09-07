800756302f50c3d4b42dcdfcc720ca3ad0ab9d90
RAKOTOBE Sitraka Harinjaka <sitrakaharinjaka@gmail.com>
2024-09-07 18:53:57 +0300

    commit intial

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <header>
+      <h1>Header</h1>
+    </header>
+    <slot />
+    <footer>
+      <h1>Footer</h1>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
